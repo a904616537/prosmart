79129001fbd22b73d1b63fa423e0eb19d9bbd782
Foowala <billy@foowala.com>
2019-04-23 03:29:58 +0800

    Fixing NHL Logo code

--- src/common/index/from.vue
+++ src/common/index/from.vue
@@ -33,8 +33,7 @@
 				<li>
 					<span><label>13.</label>有竞争就有对手，选出你不喜欢的球队吧！</span>
 					<div class="rounds">
-						<div v-for="(item, index) in teams" :key="index" class="round" :class="{active : item == data.hate}" @click="onHate(item)">
-							<img v-bind:src="'static/imgs/' + item">
+						<div v-for="(item, index) in teams" :key="index" class="round" :class="{active : item == data.hate}" @click="onHate(item) style="background-image:url('static/imgs/' + {{item}})"  ">
 						</div>
 					</div>
 				</li>
